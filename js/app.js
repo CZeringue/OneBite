4dda28a4132dbb2e67a03847f1f5f9cab8f16f0f
CZeringue <cczeringue@icloud.com>
2015-11-20 14:10:34 -0800

    update

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 $(function() { //waits until page is loaded
 $('#add').on('click',function() {
 	var itemadd = $('input').val(); //finds the value of the input box.
-		$('.item:first-child').after('<div class ="item"><i id="add" class="fa fa-square-o"></i><span><i class="fa fa-trash"></i></span><p>'+ $(itemadd).html($('input:text').val()) +'</p></div>');
+		$('.item:first-child').after('<div class ="item"><i id="add" class="fa fa-square-o"></i><span><i class="fa fa-trash"></i></span><p>'+ $(itemadd) +'</p></div>');
 	
 
 });
