6bc4763c6f3744faf2d08ca624f64df5173586b6
CZeringue <cczeringue@icloud.com>
2015-11-20 13:41:29 -0800

    Added textbox, attempting to add new list items

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+
+$(function() { //waits until page is loaded
+$('#add').on('click',function() {
+	var itemadd = $('#add').val(); //finds the value of the input box.
+		$('.item:first-child').after('<div class ="item"><i id="add" class="fa fa-square-o"></i><span><i class="fa fa-trash"></i></span><p>'+ $(itemadd).html($('input:text').val()) +'</p></div>');
+	
+
+});
+$('i .fa-trash').css('color','red');
+	});
+
+
