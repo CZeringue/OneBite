1491d2520e0c7d2da16c8fa1549e8859bca77951
CZeringue <cczeringue@icloud.com>
2015-11-20 18:06:28 -0800

    finished project

--- js/app.js
+++ js/app.js
@@ -3,27 +3,29 @@
 $(function() { //waits until page is loaded
 $('#add').on('click',function() {
 	var itemadd = $('input').val(); //finds the value of the input box.
-		$('.item:first-child').after('<div class ="item"><i class="fa fa-square-o"></i><span><i class="fa fa-trash"></i></span><p>'+ itemadd +'</p></div>');
-	
+		$('.item:first-child').after('<div class ="item"><i class="fa fa-square-o"></i><span><i class="fa fa-trash"></i></span><p>'+itemadd+'</p></div>');	
+});
 
+$('input').on('keydown', function(e){
+	if(e.keyCode == 13){ //senses use of enter button
+		$('#add').click();
+	}
 });
-$('span').on('click',function() {
+
+$('div').on('click', '.fa-trash', function() {
 	$(this).closest('div').remove();
-});
+}); //deletes items
 
 $('.list').on('click', '.fa-square-o', function() {
 	$(this).toggleClass('fa-check-square-o fa-square-o');
 	$(this).closest('div').find('p').toggleClass('checked none'); 
-});
+}); //toggles unchecked
 
 $('.list').on('click', '.fa-check-square-o', function() {
 	$(this).toggleClass('fa-square-o fa-check-square-o');
 	$(this).closest('div').find('p').toggleClass('none checked'); 
 
+});//toggles checked
+		
 });
-	
-	
-});
-
-
-
+//end
